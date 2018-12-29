968852d6724516b439f99cbbf0f89e5165393bb3
weeturn <sasquatch110@gmail.com>
2018-12-29 16:55:10 +0800

    article controller

---
+++ controller/ArticleController.py
@@ -0,0 +1,66 @@
+from flask import url_for, request
+from flask_login import logout_user, login_user, login_required
+from werkzeug.utils import redirect
+
+from controller.absract import BaseController
+from model.Article import Article
+from model.User import User
+
+
+class ArticleController(BaseController):
+    @staticmethod
+    def new_article():
+        if request.method == 'GET':
+            return '''
+         <form action='' method='POST'>
+         <input type='text' name='title' id='title' placeholder='title'/>
+         <input type='text' name='content' id='content' placeholder='content'/>
+         <input type='submit' name='submit'/>
+         </form>
+                      '''
+        else:
+            title = request.form["title"]
+            content = request.form["content"]
+            author_id = 1
+            board_id = 1
+            article = Article.create(title=title, content=content, author_id=author_id, board_id=board_id)
+            if type(article) == str:
+                param = {"error": article}
+                return article
+            return "success"
+
+    @staticmethod
+    def change_article():
+        if request.method == 'GET':
+            return '''
+         <form action='' method='POST'>
+         <input type='text' name='title' id='title' placeholder='title'/>
+         <input type='text' name='content' id='content' placeholder='content'/>
+         <input type='submit' name='submit'/>
+         </form>
+                      '''
+        if request.method == 'POST':
+            title = request.form["title"]
+            content = request.form["content"]
+            author_id = 1
+            board_id = 1
+            id = 1
+            article = Article.change(title=title, content=content, author_id=author_id, board_id=board_id, id=id)
+            if type(article) == str:
+                param = {"error": article}
+                return article
+            return "success"
+        else:
+            article = Article.delete(1)
+            if type(article) == str:
+                param = {"error": article}
+                return article
+            return "success"
+
+    @classmethod
+    def setupUrl(cls):
+        from app import app
+        app.add_url_rule(rule='/newarticle', view_func=cls.new_article,
+                         methods=["POST", "GET"])
+        app.add_url_rule(rule='/changearticle', view_func=cls.change_article,
+                         methods=["POST", "GET", "DELETE"])
