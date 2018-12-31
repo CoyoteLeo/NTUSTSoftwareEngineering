919c60b89a17b3754af5ed143fc48b0f26fb4772
Leo <karta09104@gmail.com>
2018-12-31 20:45:29 +0800

    remove redundant

--- alembic_migrations/env.py
+++ alembic_migrations/env.py
@@ -2,7 +2,6 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-import env_production
 
 # add the model path to sys.path
 import sys
@@ -44,7 +43,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = env_production.DATABASE_URL
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True)
 
--- model/BaseModel.py
+++ model/BaseModel.py
@@ -46,7 +46,9 @@ class BaseModel(AbstractConcreteBase, Base):
 
     @classmethod
     def filter(cls, order_by=None, **kwargs):
-        return session.query(cls).filter_by(**kwargs)
+        result = session.query(cls).filter_by(**kwargs)
+        session.commit()
+        return result
 
     @classmethod
     def all(cls):
@@ -54,7 +56,9 @@ class BaseModel(AbstractConcreteBase, Base):
 
     @classmethod
     def exist(cls, **kwargs):
-        return session.query(literal(True)).filter(session.query(cls).filter_by(**kwargs).exists()).scalar()
+        result = session.query(literal(True)).filter(session.query(cls).filter_by(**kwargs).exists()).scalar()
+        session.commit()
+        return result
 
     def save(self):
         session.merge(self)
@@ -63,55 +67,3 @@ class BaseModel(AbstractConcreteBase, Base):
     def delete(self):
         session.delete(self)
         session.commit()
-
-# # 5. 查询数据
-# # 5.1 返回结果集的第二项
-# user = session.query(User).get(2)
-#
-# # 5.2 返回结果集中的第2-3项
-# users = session.query(User)[1:3]
-#
-# # 5.3 查询条件
-# user = session.query(User).filter(User.id < 6).first()
-#
-# # 5.4 排序
-# users = session.query(User).order_by(User.name)
-#
-# # 5.5 降序（需要导入desc方法）
-# from sqlalchemy import desc
-#
-# users = session.query(User).order_by(desc(User.name))
-#
-# # 5.6 只查询部分属性
-# users = session.query(User.name).order_by(desc(User.name))
-# for user in users:
-#     print
-#     user.name
-#
-# # 5.7 给结果集的列取别名
-# users = session.query(User.name.label('user_name')).all()
-# for user in users:
-#     print
-#     user.user_name
-#
-# # 5.8 去重查询（需要导入distinct方法）
-# from sqlalchemy import distinct
-#
-# users = session.query(distinct(User.name).label('name')).all()
-#
-# # 5.9 统计查询
-# user_count = session.query(User.name).order_by(User.name).count()
-# age_avg = session.query(func.avg(User.age)).first()
-# age_sum = session.query(func.sum(User.age)).first()
-#
-# # 5.10 分组查询
-# users = session.query(func.count(User.name).label('count'), User.age).group_by(User.age)
-# for user in users:
-#     print
-#     'age:{0}, count:{1}'.format(user.age, user.count)
-#
-# # 6.1 exists查询(不存在则为~exists())
-# from sqlalchemy.sql import exists
-#
-# session.query(User.name).filter(~exists().where(User.role_id == Role.id))
-# # SELECT name AS users_name FROM users WHERE NOT EXISTS (SELECT * FROM roles WHERE users.role_id = roles.id)
