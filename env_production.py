0bf900b86778d902ebed5718c4e21f7c3108e670
Leo <karta09104@gmail.com>
2019-01-05 19:17:50 +0800

    change database

--- env_production.py
+++ env_production.py
@@ -1,4 +1,5 @@
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://se:se@localhost/se")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://se:se@localhost:5432/se")
+"mysql://se:se@localhost:3306/se"
 DEBUG = True
